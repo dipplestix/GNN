56eef002efd5d057bb313faceb58844a85f27a0c
dipplestix <chrism315@gmail.com>
2023-04-29 17:05:50 -0400

    Expanded loss functions

--- utilities/eval.py
+++ utilities/eval.py
@@ -15,3 +15,14 @@ def dot_product_loss(col_probs, edge_list):
     adj = to_dense_adj(edge_list)
     loss = torch.sum(torch.mm(col_probs, col_probs.T) * adj)/2
     return loss
+
+
+def self_info_loss(col_probs):
+    log_probs = torch.log(col_probs)
+    loss = torch.trace(torch.mm(col_probs, log_probs.T))
+    return loss
+
+def wang_loss_function(col_probs, edge_list, lam):
+    # Loss function from Wang's 2023 paper: A Graph Neural Network with Negative Message Passing for Graph Coloring
+    loss = dot_product_loss(col_probs, edge_list) + lam*self_info_loss(col_probs)
+    return loss
